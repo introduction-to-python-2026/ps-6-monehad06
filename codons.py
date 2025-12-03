bce0115891846625f7c30fc26b69befb7726ba4a
monehad06 <mo.nehad06@gmail.com>
2025-12-03 12:41:43 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,13 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
 
+    codon_dict = {}  # מילון ריק
+    with open(file_path, 'r') as file: 
+        rows = file.readlines()          
 
+    for row in rows:
+        cells = row.strip().split('\t')  
+        codon = cells[0]                
+        amino_acid = cells[2]            
+        codon_dict[codon] = amino_acid  
+
+    return codon_dic
